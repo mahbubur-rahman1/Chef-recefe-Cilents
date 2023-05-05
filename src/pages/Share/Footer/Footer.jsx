7bd0a5cc00aab91b2071f9a82ea2ee808006721f
mahbubur-rahman1 <you@example.com>
2023-05-05 23:45:53 +0600

    fota

--- src/pages/Share/Footer/Footer.jsx
+++ src/pages/Share/Footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
     <a className="link link-hover">About us</a> 
     <a className="link link-hover">Contact</a> 
     <a className="link link-hover">Jobs</a> 
-    <a className="link link-hover">Press kit</a>
+    <a className="link link-hover">Pres kit</a>
   </div> 
   <div>
     <span className="footer-title">Legal</span> 
